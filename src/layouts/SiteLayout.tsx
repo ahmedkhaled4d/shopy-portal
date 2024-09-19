8b420ce50bea624787018fe34b3378be2cd0cc1e
Ahmed Khaled <ahmedkhaled4d@gmail.com>
2024-09-19 15:06:46 +0300

    Reviews

--- src/layouts/SiteLayout.tsx
+++ src/layouts/SiteLayout.tsx
@@ -1,10 +1,14 @@
 import { Outlet } from "react-router-dom";
+import Header from "../components/site/Header";
+import Footer from "../components/site/Footer";
 
 const SiteLayout = () => {
   return (
     <main>
       <div className="">
+        <Header />
         <Outlet />
+        <Footer />
       </div>
     </main>
   );
